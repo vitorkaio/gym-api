14b2294cff2931d5c2b7055deba0156eea47a77a
vitorkaio <vitorcaiodepaula@gmail.com>
2020-01-08 13:18:30 -0300

    add url mongodb atlas

--- src/config/env_test.js
+++ src/config/env_test.js
@@ -1,3 +1,3 @@
-export const mongoUrl = 'mongodb://localhost:27017/gym'
+export const mongoUrl = 'mongodb+srv://shan:abcabc123@nutri-laiso.mongodb.net/gym-api?retryWrites=true&w=majority'
 export const mongoUrlTest = 'mongodb://localhost:27017/cars-api'
 export const port = process.env.PORT || 8080
