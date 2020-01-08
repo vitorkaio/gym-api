0961381fe46d220794b89cdcfb18c42997025dc1
vitorkaio <vitorcaiodepaula@gmail.com>
2020-01-08 13:02:28 -0300

    port update

--- src/config/env_test.js
+++ src/config/env_test.js
@@ -1,3 +1,3 @@
 export const mongoUrl = 'mongodb://localhost:27017/gym'
 export const mongoUrlTest = 'mongodb://localhost:27017/cars-api'
-export const port = process.env.PORT || 3333
+export const port = process.env.PORT || 8080
