5bd699e69dfdc4bf864d5fcf4b22fd2667a06b72
vitorkaio <vitorcaiodepaula@gmail.com>
2020-01-14 08:36:58 -0300

    update api

--- src/config/env_test.js
+++ src/config/env_test.js
@@ -1,3 +1,3 @@
-export const mongoUrl = 'mongodb+srv://shan:abcabc123@nutri-laiso.mongodb.net/gym-api?retryWrites=true&w=majority'
-// export const mongoUrl = 'mongodb://localhost:27017/gym'
-export const port = process.env.PORT || 8080
+// export const mongoUrl = 'mongodb+srv://shan:abcabc123@nutri-laiso.mongodb.net/gym-api?retryWrites=true&w=majority'
+export const mongoUrl = 'mongodb://localhost:27017/gym'
+export const port = process.env.PORT || 3333
